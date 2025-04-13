8ad1bc4c3681fa4b016f225a47f2f50cb5542866
mahdi <mahdi.me.14.m@gmail.com>
2025-04-13 22:57:20 +0330

    fixed publishPath

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/linuxsystemshop/' : '/'
+publicPath:"/linuxsystemshop/",
+  outputDir:'dist'
 };
